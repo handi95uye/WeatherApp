e496d7d5e1438c74b2bf61225b040402f560051b
hareshkh <khanna.haresh@yahoo.co.in>
2016-04-07 00:19:11 +0530

    Completes the Basic Functionality

--- app/src/main/java/com/example/haresh/retrofitlearning/MainActivity.java
+++ app/src/main/java/com/example/haresh/retrofitlearning/MainActivity.java
@@ -16,38 +16,50 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
     private String TAG = "MainAcitvity";
-    Button refresh;
+    Button refreshButton;
     TextView city, date, temperature, conditions;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        refresh = (Button) findViewById(R.id.refreshButton);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        refreshButton = (Button) findViewById(R.id.refreshButton);
         city = (TextView) findViewById(R.id.city);
         date = (TextView) findViewById(R.id.date);
         temperature = (TextView) findViewById(R.id.temperature);
         conditions = (TextView) findViewById(R.id.conditions);
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        refresh.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        WeatherApi.Factory.getInstance().getWeather().enqueue(new Callback<Weather>() {
-                            @Override
-                            public void onResponse(Call<Weather> call, Response<Weather> response) {
-                                temperature.setText(response.body().getQuery().getResults().getChannel().getItem().getCondition().getTemp());
-                                city.setText(response.body().getQuery().getResults().getChannel().getLocation().getCity());
-                                date.setText(response.body().getQuery().getResults().getChannel().getLastBuildDate());
-                                conditions.setText(response.body().getQuery().getResults().getChannel().getItem().getCondition().getText());
-                            }
 
-                            @Override
-                            public void onFailure(Call<Weather> call, Throwable t) {
-                                Log.e(TAG,t.getMessage());
-                            }
-                        });
-                    }
+        refreshButton.setOnClickListener(
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    change();
                 }
+            }
         );
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        change();
+    }
+
+    private void change() {
+        WeatherApi.Factory.getInstance().getWeather().enqueue(new Callback<Weather>() {
+            @Override
+            public void onResponse(Call<Weather> call, Response<Weather> response) {
+                temperature.setText(response.body().getQuery().getResults().getChannel().getItem().getCondition().getTemp());
+                city.setText(response.body().getQuery().getResults().getChannel().getLocation().getCity());
+                date.setText(response.body().getQuery().getResults().getChannel().getLastBuildDate());
+                conditions.setText(response.body().getQuery().getResults().getChannel().getItem().getCondition().getText());
+            }
+
+            @Override
+            public void onFailure(Call<Weather> call, Throwable t) {
+                Log.e(TAG, t.getMessage());
+            }
+        });
     }
 }
