1bc8af6fac77e12e9210ca67c25d2bc158a6c48b
hareshkh <khanna.haresh@yahoo.co.in>
2016-04-06 17:47:35 +0530

    Adds Retrofit Interface

--- app/src/main/java/com/example/haresh/retrofitlearning/MainActivity.java
+++ app/src/main/java/com/example/haresh/retrofitlearning/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.haresh.retrofitlearning;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-//https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22roorkee%2C%20india%22)%20and%20u%3D'c'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys
 public class MainActivity extends AppCompatActivity {
 
     @Override
---
+++ app/src/main/java/com/example/haresh/retrofitlearning/data/remote/WeatherApi.java
@@ -0,0 +1,32 @@
+package com.example.haresh.retrofitlearning.data.remote;
+
+import com.example.haresh.retrofitlearning.data.model.Weather;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.http.GET;
+
+/**
+ * Created by Haresh on 06-04-2016.
+ */
+public interface WeatherApi {
+
+    String BASE_URL = "https://query.yahooapis.com/v1/public/";
+    @GET("yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22roorkee%2C%20india%22)%20and%20u%3D'c'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys")
+    Call<Weather> getWeather();
+
+    class Factory {
+
+        private static WeatherApi service;
+        public static WeatherApi getInstance() {
+            if (service == null) {
+                Retrofit retrofit = new Retrofit.Builder().baseUrl(BASE_URL).build();
+                service = retrofit.create(WeatherApi.class);
+                return service;
+            }
+            else {
+                return service;
+            }
+        }
+    }
+}
